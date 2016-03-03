fd6ec6f30827d349974b0e12aa4da9e549103959
Maksym Davydov <mdavydov@adva-soft.com>
2016-03-03 20:10:12 +0200

    + to .

--- shop-step3.php
+++ shop-step3.php
@@ -2,9 +2,10 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$outp = '[ {"name":"Smile 1","icon":"smile1.svg", "desc":"This is a very cool smile", "price":0.99},'+
-          '{"name":"Smile 2","icon":"smile2.svg", "desc":"Another cool smile", "price":2.99},' +
+$outp = '[ {"name":"Smile 1","icon":"smile1.svg", "desc":"This is a very cool smile", "price":0.99},'.
+          '{"name":"Smile 2","icon":"smile2.svg", "desc":"Another cool smile", "price":2.99},'.
           '{"name":"Smile 3","icon":"smile3.svg", "desc":"Smiles continued with <p> :-)...", "price":3.99} ]';
 
 echo($outp);
 ?>
+

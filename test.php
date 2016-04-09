e9df21cefa0e287caf6955efab42c2acdbfb6573
Maksym Davydov <mdavydov@adva-soft.com>
2016-04-09 14:06:10 +0300

    sqlsrv fix

--- test.php
+++ test.php
@@ -27,7 +27,7 @@
     try
     {
         echo "Conn str = ".getenv("SQLAZURECONNSTR_defaultConnection");
-        $conn = new PDO( getenv("SQLAZURECONNSTR_defaultConnection") );
+        $conn = new PDO( "sqlsrv:".getenv("SQLAZURECONNSTR_defaultConnection") );
         
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         

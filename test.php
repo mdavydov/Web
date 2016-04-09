28ca63839f274b65fc3c13230a1baa3a4e383870
Maksym Davydov <mdavydov@adva-soft.com>
2016-04-09 11:53:00 +0300

    Test database connection

--- test.php
+++ test.php
@@ -1,5 +1,28 @@
-<?
+<?php
 
- var_dump($_ENV);
+    echo htmlspecialchars("Hello");
+ 
+    try
+    {
+        foreach($_ENV as $k => $v)
+        {
+            echo htmlspecialchars($k. " => ". $v)."<br>";
+        }
+    }
+    catch(Exception $err)
+    {
+        echo $err->getMessage();
+    }
+ 
+    try
+    {
+        $conn = new PDO ( "sqlsrv:server = tcp:mdavydovlab4.database.windows.net,1433; Database = MDavydovLab4", "mdavydov", "{your_password_here}");
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch ( PDOException $e )
+    {
+        print( "Error connecting to SQL Server." );
+        die(print_r($e));
+    }
 
 ?>

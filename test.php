81ff42b6b6ec293f94a192f6099015026ac950e6
Maksym Davydov <mdavydov@adva-soft.com>
2016-04-16 21:21:36 +0300

    test4

--- test.php
+++ test.php
@@ -51,9 +51,9 @@
                  "ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,".
                  "firstname     VARCHAR( 64 ) NOT NULL,".
                  "lastname      VARCHAR( 64 ) NOT NULL,".
-                 "email         VARCHAR( 64 ) PRIMARY KEY,".
+                 "email         VARCHAR( 64 ) NOT NULL UNIQUE,".
                  "password      VARCHAR( 128 ) NOT NULL,".
-                 "admin         BIT".
+                 "admin         BIT  NOT NULL".
                  ")";
         
         try {
